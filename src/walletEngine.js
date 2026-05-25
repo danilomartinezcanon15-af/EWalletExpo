fc228f15601244d61e24bf0f42d2c1c8680200e1
Xavizooo <72javito24@gmail.com>
2026-05-24 23:06:35 -0500

    wallEngine

---
+++ src/walletEngine.js
@@ -0,0 +1,9 @@
+import { fakerES_MX as faker } from '@faker-js/faker';
+
+const transactionTypes = ['Ingreso', 'Retiro'];
+
+const transactionStatus = [
+  'Completado',
+  'Pendiente',
+  'Rechazado',
+];
