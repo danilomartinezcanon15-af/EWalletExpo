496abadfad973db63e6e3fe0a508b6474f5fbfa5
danilomartinezcanon15-af <danilomartinezcanon15@gmail.com>
2026-05-24 22:24:27 -0500

    Dependencias y constantes compartidas

--- src/walletEngine.js
+++
@@ -1,9 +0,0 @@
-import { fakerES_MX as faker } from '@faker-js/faker';
-
-const transactionTypes = ['Ingreso', 'Retiro'];
-
-const transactionStatus = [
-  'Completado',
-  'Pendiente',
-  'Rechazado',
-];
